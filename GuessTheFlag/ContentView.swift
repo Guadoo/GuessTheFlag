ecc33a550d3e55ab55a384130f0e29fa25874012
Liu Jingkai <rock_jk@msn.com>
2021-05-19 16:23:55 +0800

    enhance by Day 34 Challenges

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -27,8 +27,6 @@ struct FlagImage: View {
     }
 }
 
-
-
 struct ContentView: View{
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
@@ -37,6 +35,12 @@ struct ContentView: View{
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
+    
+    @State private var animationRotation = 0.0
+    @State private var animationZoom: CGFloat = 1
+    
+    @State private var isOpacity = false
+    @State private var animationOpacity = 1.0
     
     var body: some View {
         ZStack {
@@ -56,6 +60,16 @@ struct ContentView: View{
                     Button(action: {
                         // flag was tapped
                         self.flagTapped(number)
+                
+                        if number == correctAnswer {
+                            withAnimation(.interpolatingSpring(stiffness: 1, damping: 2)){
+                                self.animationRotation += 360
+                            }
+                        } else {
+                            withAnimation(.easeInOut) {
+                                self.animationZoom = 1.4
+                            }
+                        }
                     }, label: {
 //------------------------------------------------------------------------------------
 // Use standard Image view
@@ -69,6 +83,9 @@ struct ContentView: View{
                         FlagImage(countries: countries, number: number)
                         
                     })
+                    .opacity(animationOpacity)
+                    .rotation3DEffect(.degrees( number == correctAnswer ? animationRotation : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
+                    .scaleEffect(number == correctAnswer ? animationZoom : 1)
                     .alert(isPresented: $showingScore, content: {
                         Alert(title: Text(scoreTitle), message: Text("Your score is \(userScore)"), dismissButton: .default(Text("Continue"), action: {
                             self.askQuestion()
@@ -78,6 +95,19 @@ struct ContentView: View{
                 
                 Text("Your Current Score is \(userScore)")
                     .foregroundColor(.white)
+                
+                Button(isOpacity ? "Opacity 0.25" : "Opacity 1.0") {
+
+                    isOpacity = !isOpacity
+                    
+                    withAnimation {
+                        animationOpacity = {isOpacity ? 0.25 : 1.0}()
+                    }
+                }
+                .padding()
+                .background(Color.black)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
                 
                 Spacer()
             }
@@ -99,6 +129,7 @@ struct ContentView: View{
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        animationZoom = 1
     }
     
 }
